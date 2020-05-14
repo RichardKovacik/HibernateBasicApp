ab3c57f8ef95b8bc13739a829be36ea731960782
RichardKovacik <56000073+RichardKovacik@users.noreply.github.com>
2020-05-14 16:00:48 +0200

    1.test JDBC connection 2.create hibernate.cfg 3.entity Student 4.save object to db with Session in App class

---
+++ src/main/java/org/example/CRUD/RetrievingObjects.java
@@ -0,0 +1,42 @@
+package org.example.CRUD;
+
+import models.Student;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.List;
+
+public class RetrievingObjects {
+    public static void main(String[] args) {
+        //creating SessionFactory
+        SessionFactory factory= new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Student.class)
+                .buildSessionFactory();
+        //create session
+        Session session=factory.getCurrentSession();
+        try {
+            //start transaction
+            session.beginTransaction();
+            //getting students
+
+            List<Student> students=session.createQuery("from models.Student ").list();
+            //display students
+            for (Student tempStudent:students) {
+                System.out.println("id:"+tempStudent.getId()+"  meno:"+tempStudent.getFirstname()+"  " +
+                        "priezvisko: "+tempStudent.getLastname());
+
+            }
+
+            //commit transaction
+            session.getTransaction().commit();
+
+        }finally {
+            factory.close();
+        }
+
+    }
+
+}
